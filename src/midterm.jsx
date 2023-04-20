9a93d7cd02d85f3919511970d5abc4a06dbd4c09
문기현 <john7266@naver.com>
2023-04-20 13:47:27 +0900

    2ndCommit

--- src/midterm.jsx
+++ src/midterm.jsx
@@ -1,2 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 
+const props = [
+    {
+        btn: "사과",
+        rs: "사과",
+    },
+    {
+        btn: "사과",
+        rs: "사과",
+    },
+    {
+        btn: "사과",
+        rs: "사과",
+    },
+];
+
+function midterm(props) {
+    return (
+        <div>
+            <div>
+                <h1>어떤 과일을 좋아하나요?</h1>
+            </div>
+            <div>
+                <span>{props.rs}</span>
+            </div>
+            <div>
+                <button>{props.btn}</button>
+                <button>{props.btn}</button>
+                <button>{props.btn}</button>
+            </div>
+        </div>
+    );
+} 
+
+export default midterm;
